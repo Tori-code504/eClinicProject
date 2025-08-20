7907ae71f01d250affaef30427dd2593c1c10581
Victoria <toriamwendeo11@gmail.com>
2025-08-20 14:05:23 +0300

    Setting up the models and admin interface

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import User, PatientProfile, Appointment, QueueEntry, MedicalRecord
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(PatientProfile)
+admin.site.register(Appointment)
+admin.site.register(QueueEntry)
+admin.site.register(MedicalRecord)
+
+
+
--- core/models.py
+++ core/models.py
@@ -1,3 +1,61 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    ROLE_CHOICES = [
+        ('admin', 'Admin'),
+        ('doctor', 'Doctor'),
+        ('nurse', 'Nurse'),
+        ('patient', 'Patient'),
+    ]
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='patient')
+    phone_number = models.CharField(max_length=15,blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.username} ({self.role})"
+    
+class PatientProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='patient_profile')
+    date_of_birth =models.DateField(null=True, blank=True)
+    gender = models.CharField(max_length=10, choices=[('male', 'Male'),('female', 'Female')])
+    national_id = models.CharField(max_length=20, unique=True)
+    address = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"Patient: {self.user.username}"
+    
+class Appointment(models.Model):
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('confirmed', 'Confirmed'),
+        ('cancelled', 'Cancelled'),
+    ]
+    patient = models.ForeignKey(User, on_delete=models.CASCADE, related_name="appointments")
+    doctor = models.ForeignKey(User, on_delete=models.CASCADE, related_name="doctor_appointments" )
+    appointment_time = models.DateTimeField()
+    service_time = models.CharField(max_length=100)
+    status = models.CharField(max_length=20, choices= STATUS_CHOICES, default='pending')
+
+    def __str__(self):
+        return f"{self.patient.username} with Dr. {self.doctor.username} on {self.appointment_time}"
+
+class QueueEntry(models.Model):
+    appointment = models.OneToOneField(Appointment, on_delete= models.CASCADE, related_name= 'queue_entry')
+    position = models.PositiveIntegerField()  
+    time_added = models.DateTimeField(auto_now_add=True) 
+
+    def __str__(self):
+        return f"Queue #{self.position} - {self.appointment.patient.username}" 
+    
+class MedicalRecord(models.Model):
+    patient = models.ForeignKey(User, on_delete=models.CASCADE, related_name="medical_records")
+    doctor = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="created_records")
+    record_date = models.DateTimeField(auto_now_add=True)
+    diagnosis = models.TextField()
+    treatment = models.TextField()
+    notes = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"Record for {self.patient.username} on {self.record_date}"
+
+
