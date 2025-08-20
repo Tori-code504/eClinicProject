ad15eec42bb8b0bc10b1f9d3329bce5bdf834181
Victoria <toriamwendeo11@gmail.com>
2025-08-20 15:16:55 +0300

    adding authentication(JWT tokens)

--- eclinic/settings.py
+++ eclinic/settings.py
@@ -51,6 +51,13 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+
 ROOT_URLCONF = 'eclinic.urls'
 
 TEMPLATES = [
